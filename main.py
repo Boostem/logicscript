38a537f0d9ec9b637f4a1ac1366159304b58c104
boostem <IlikeGH)&1>
2021-04-28 18:21:10 -0400

    Init COmmit

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-
+import random
 class Node:
     def __init__(self, name):
-        self.name = name
         self.sa0 = 0
+        self.name = name
         self.sa1 = 0
         self.testability = 0
 
+
 gatelist = []  # list of gates
 names = []  # list of names
+keys = [] # keys to keep track of operations
 
-
-def gates():
+def readgates():
     with open("c17.fault") as f:
         for line in f:
-            # skip the wires, they are redundant and skip tests
+            # Used to grab specific information by skipping some wires
             #if "->" in line:
             #    continue
             if "test" in line:
-                continue
+                temp = line.split()
+                goodoutput = temp[3]
             if "*" not in line:
                 continue
 
             data = line.split()
-            print(data)
+            #print(data)
 
             #get object list going
             if data[0] not in names:
@@ -39,13 +41,18 @@ def gates():
                 elif "/1" in data[1]:
                     localsa1 += 1
 
-            #search for object in list, add to their Stuck-At values
-            for index, g in enumerate(gatelist):
-                if g.name == data[0]:
-                    g.sa1 += localsa1
-                    g.sa0 += localsa0
-            else: #To keep compiler happy
-                index = -1
+                #search for object in list, add to their Stuck-At values
+                for index, g in enumerate(gatelist):
+                    if g.name == data[0]:
+                        g.sa1 += localsa1
+                        g.sa0 += localsa0
+                        #g.testability = g.sa1 + g.sa0
+                        #print(str(g.name) + ": goodoutput is: " + str(goodoutput) + " badoutput is: " + str(data[3]))
+                        #print("hd is: " + str(hd(int(goodoutput,2), int(data[3],2))))
+                        g.testability = g.sa1*hd(int(goodoutput,2), int(data[3],2)) + g.sa0*hd(int(goodoutput,2), int(data[3],2))
+                else: #To keep compiler happy??
+                    index = -1
+        gatelist.sort(key=lambda x: x.testability, reverse=True)
 
 
 # Function to calculate hamming distance
@@ -59,149 +66,75 @@ def hd(n1, n2):
     return setBits
 
 
-if __name__ == "__main__":
-    gates()
-    for g in gatelist:
-        print((g.name) + " test is: " + str(g.sa0 + g.sa1))
-
-    #print(hd(0b0101, 0b1010))
-
-'''import re
-import numpy as np
-
-
-class Node:
-    def __init__(self, name, sa0, sa1):
-        self.name = name
-        self.sa0 = sa0
-        self.sa1 = sa1
-        self.testability = self.sa0 + self.sa1
-nodes.sort(key=lambda node: node.testability, reverse=True)
-
-class LogicObf:
-    def __init__(self, netlist_filename, output_filename, num_keys):
-        self.netlist_filename = netlist_filename
-        self.output_filename = output_filename
-        self.output_file = open(self.output_filename, "r")
-        self.netlist_file = open(self.netlist_filename, "r")
-        self.nodes = list()
-        self.num_keys = num_keys
-
-    def run(self):
-        self.create_nodes()
-        self.sort_nodes()
-        self.add_key_gates()
-
-    def create_nodes(self):
-        for line in self.output_file:
-            if line == '\n':
-                break
-            name = line.rstrip()
-            next(self.output_file)
-            next(self.output_file)
-            sa0 = int(next(self.output_file).split("T(sa0): ")[1].rstrip())
-            sa1 = int(next(self.output_file).split("T(sa1): ")[1].rstrip())
-            self.nodes.append(Node(name, sa0, sa1))
-
-    def sort_nodes(self):
-        nodes = self.nodes
-        nodes.sort(key=lambda node: node.testability, reverse=True)
-        self.nodes = nodes
-
-    def get_nodes(self):
-        return self.nodes
-
-    def add_key_gates(self):
-        file = self.netlist_file.readlines()
-        for i in range(self.num_keys):
-            file.append('INPUT(key' + str(i) + ')')
-            node = self.nodes[i]
-            if '_' in node.name:
-                keygate_input = node.name.split('_')[0]
-                keygate_output = node.name.split('_')[1].split('[')[1].split(']')[0]
-                keygate_type = np.random.choice(['XOR', 'XNOR'])
-                file.append('keygate' + str(i) + ' = ' + keygate_type + '(key' + str(i) + ', ' + keygate_input + ')')
-                # modify the line to take keygate as input
-                for line in range(len(file)):
-                    if file[line].split('=')[0].split(' ')[0] == keygate_output:
-                        match = keygate_input + "(?P<character>\,|\))"
-                        regex = re.compile(r"" + match)
-                        file[line] = regex.sub(r"" + "keygate" + str(i) + "\g<character>", file[line])
-                        break
-            else:
-                keygate_type = np.random.choice(['XOR', 'XNOR'])
-                keygate_input = node.name.split(':')[0]
-                for line in range(len(file)):
-                    match = keygate_input + "(?P<character>\,|\))"
-                    regex = re.compile(r"" + match)
-                    file[line] = regex.sub(r"" + "keygate" + str(i) + "\g<character>", file[line])
-                file.append('keygate' + str(i) + ' = ' + keygate_type + '(key' + str(i) + ', ' + keygate_input + ')')
-        with open(self.netlist_filename.split('.')[0] + "_obf.bench", 'w') as f:
-            for line in file:
-                f.write("%s\n" % line.upper())
-
-
-def main():
-    logic_obf = LogicObf("c1196.bench", "c1196_SCOAP_Output.txt", 30)
-    logic_obf.run()
-    nodes = logic_obf.get_nodes()
-
-
-if __name__ == "__main__":
-    main()'''
-
-'''class Node:
-    def __init__(self, name):
-        self.name = name
-        self.sa0 = 0
-        self.sa1 = 0
-
-tests = []
-nodes = []
-nodenames = [] #list of names for checking
-def main():
-    left = 0b1111
-    right= 0b1011
-    new = left ^ right
-    count=0
-    while(new):
-        print(count)
-        count = count + (new & 1)
-        new>>=1
-    print(count)
-    with open("c17.fault") as f:
-        for line in f:
-            if "->" in line:
-                continue
-            data = line.split()
-            #print(data)
-            if data[0] == "test":
-                print("")
-                #print("test: " + data[1] + " with output: " + data[3])
-
-            if "gat" in data[0]:
-                name = data[0]
-                if name not in nodenames:
-                    n = Node(name)
-                else:
+def addgates(numkeys):
+    file = open("c17.bench", "r").readlines()
+    ofile = open("c17obf.bench", "w+")
+    i=0
+    for num in range(numkeys):
+        gate = gatelist[i]
+        if "->" in gate.name:
+            kinput = gate.name.split('->')[0]
+            koutput = gate.name.split('->')[1]
+            #print(kinput + " -> " + koutput)
+            for line in range(len(file)):
+                if file[line].split('=')[0].split(' ')[0] == koutput:
+                    #print("found -> gate")
+                    #print(file[line])
+                    keyginsert = "KEYGATE" + str(num)
+                    file[line] = file[line].replace(kinput, keyginsert)
+                elif "~" in file[line]:
+                    #dont wanna mess up new key/keygates
                     break
-                if "*" in data[2]:
-                    if "/0" in data[1]:
-                        n.sa0 += 1
-                        #print(name + " is stuck at 0 with output " + data[3])
-                    elif "/1" in data[1]:
-                        n.sa1 += 1
-                        #print(name + " is stuck at 1 with output " + data[3])
-                nodes.append(n)
+                if num == numkeys-1:
+                    ofile.write(file[line])
+        else:
+            for line in range(len(file)):
+                kinput = gate.name
+                if kinput in file[line]:
+                    if not file[line].split('=')[0].split(' ')[0] == kinput:
+                        #if not file[line].startswith("O") | file[line].startswith("I"):
+                            #print(file[line])
+                            #print(kinput)
+                            # print(file[line])
+                        keyginsert = "KEYGATE" + str(num)
+                        file[line] = file[line].replace(kinput, keyginsert)
+                    #match = kinput + "(?P<character>\,|\))"
+                    #regex = re.compile(r"" + match)
+                    #file[line] = regex.sub(r"" + "KEYGATE" + str(i) + "\g<character>", file[line])
 
+                    #print(file[line])
+                    #keyginsert = "KEYGATE" + str(num)
+                    #file[line] = file[line].replace(kinput, keyginsert)
+                elif '~' in file[line]:
+                    # dont wanna mess up new key/keygates
+                    break
+                if num == numkeys-1:
+                    ofile.write(file[line])
+        i+=1
+    ofile.close()
 
+    ##add keys
+    file = open("c17obf.bench", "a")
+    file.write("~\n")
+    for i in range(numkeys):
+        if "->" in gatelist[i].name:
+            gatez = gate.name.split('->')[0]
+        else:
+            gatez = gatelist[i].name
+        file.write("INPUT(KEY" + str(i) + ")\n")
+        op = random.choice(["XOR(", "XNOR("])
+        keys.append(op)
+        file.write("KEYGATE" + str(i) + " = " + str(op) + "KEY" + str(i) + ", " + str(gatez) + ")\n")
+    file.close()
 
-   for no in nodes:
-        print(no.name)
-        print(no.sa0)
-        print(no.sa1)
 
 
 
 if __name__ == "__main__":
-    main()'''
+    readgates()
+    for g in gatelist:
+        print(g.name + " testa: " + str(g.testability))
+
+    numkeys=3
+    addgates(numkeys)
+
